32ae2ee6d510f95b5bff7537d0a01b4689469cdd
SergiyIlchyshyn <sergiy.ilchyshyn@gmail.com>
2017-10-12 22:08:29 +0300

    create form

--- js/script.js
+++ js/script.js
@@ -5,14 +5,29 @@ $(document).ready(function () {
     $("#arrow").on("click", "a", function(event) {
         //отменяем стандартную обработку нажатия по ссылке
         event.preventDefault();
-
         //забираем идентификатор бока с атрибута href
         var id = $(this).attr('href'),
-
         //узнаем высоту от начала страницы до блока на который ссылается якорь
             top = $(id).offset().top;
-
         //анимируем переход на расстояние - top за 1..0 мс
         $('body,html').animate({scrollTop: top}, 1500);
     }); // закінчення on
+
+    // Initializing popup
+    $(".button").magnificPopup();
+
+    // Ajax відправка форми
+    $("#form").submit(function(){
+        $.ajax({
+            type: "POST",
+            url: "mail.php",
+            data: $(this).serialize()
+        }).done(function(){
+            alert("Дякую за заявку! Найближчим часом з Вами зв'яжемося.");
+            setTimeout(function(){
+                $.magnificPopup.close();
+            }, 1000); // закінчення setTimeout
+        }); // закінчення ajx
+        return false;
+    }); // закінчення form
 });     // закінчення ready
