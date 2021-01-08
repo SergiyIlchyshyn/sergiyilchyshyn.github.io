4fa84cbd41a5efb840876a3c32a498b727eaa56c
SergiyIlchyshyn <sergiy.ilchyshyn@gmail.com>
2021-01-08 18:50:39 +0200

    pizzatime updated

--- works/pizzatime/js/main.js
+++ works/pizzatime/js/main.js
@@ -1,5 +1,32 @@
 /* libs start */ ;
 (function() {
+    var canUseWebP = function() {
+        var elem = document.createElement('canvas');
+
+        if (!!(elem.getContext && elem.getContext('2d'))) {
+            // was able or not to get WebP representation
+            return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0;
+        }
+
+        // very old browser like IE 8, canvas not supported
+        return false;
+    };
+
+    var isWebpSupported = canUseWebP();
+
+    if (isWebpSupported === false) {
+        var lazyItems = document.querySelectorAll('[data-src-replace-webp]');
+
+        for (var i = 0; i < lazyItems.length; i += 1) {
+            var item = lazyItems[i];
+
+            var dataSrcReplaceWebp = item.getAttribute('data-src-replace-webp');
+            if (dataSrcReplaceWebp !== null) {
+                item.setAttribute('data-src', dataSrcReplaceWebp);
+            }
+        }
+    }
+
     var lazyLoadInstance = new LazyLoad({
         elements_selector: ".lazy"
     });
