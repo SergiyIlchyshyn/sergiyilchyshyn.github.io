4878d2faadd37836b37b3ee1f343acca265d6944
Sergiy Ilchyshyn <sergiy.ilchyshyn@gmail.com>
2021-02-14 04:18:32 +0200

    updated porsh-marsh

--- works/porsh-marsh/assets/js/main.js
+++ works/porsh-marsh/assets/js/main.js
@@ -1,2 +1,9 @@
-$('.input-container').last().css("margin-bottom", "0");
-$('.form__text').last().css("margin-top", "50px");
+$(function() {
+    // DateTimePicker
+    flatpickr('#flatpickr', {
+        "mode": "range"
+    });
+    // =================================================
+    $('.input-container').last().css("margin-bottom", "0");
+    $('.form__text').last().css("margin-top", "50px");
+});
