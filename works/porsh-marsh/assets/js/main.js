b1ae73a5fa8e3fbf9334a92c71b3be5b3366c63d
Sergiy Ilchyshyn <sergiy.ilchyshyn@gmail.com>
2021-02-15 00:25:03 +0200

    updated dashboard

--- works/porsh-marsh/assets/js/main.js
+++ works/porsh-marsh/assets/js/main.js
@@ -1,9 +1,21 @@
-$(function() {
+$(document).ready(function() {
     // DateTimePicker
     flatpickr('#flatpickr', {
         "mode": "range"
     });
+
     // =================================================
     $('.input-container').last().css("margin-bottom", "0");
     $('.form__text').last().css("margin-top", "50px");
+
+    // ================= Tabs ==========================
+    $('ul.tabs__menu li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs__menu li').removeClass('current');
+		$('.tab').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	});
 });
