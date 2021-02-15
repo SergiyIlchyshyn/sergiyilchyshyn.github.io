8783b8f618ed95c5af9b8ffafc7c074ddc575870
Sergiy Ilchyshyn <sergiy.ilchyshyn@gmail.com>
2021-02-15 02:11:35 +0200

    added chart & updated dashboard

--- works/porsh-marsh/assets/js/main.js
+++ works/porsh-marsh/assets/js/main.js
@@ -9,13 +9,47 @@ $(document).ready(function() {
     $('.form__text').last().css("margin-top", "50px");
 
     // ================= Tabs ==========================
-    $('ul.tabs__menu li').click(function(){
+    $('#tabs-menu-1 li').click(function(){
 		var tab_id = $(this).attr('data-tab');
 
-		$('ul.tabs__menu li').removeClass('current');
-		$('.tab').removeClass('current');
+		$('#tabs-menu-1 li').removeClass('current');
+		$('.tab_1').removeClass('current');
 
 		$(this).addClass('current');
 		$("#"+tab_id).addClass('current');
 	});
+    $('#tabs-menu-2 li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('#tabs-menu-2 li').removeClass('current');
+		$('.tab_2').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	});
+    // =============== Chart.js ===========
+    new Chart(document.getElementById("line-chart"), {
+        type: 'line',
+        data: {
+          labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+          datasets: [{ 
+              data: [86,114,106,106,107,111,133,221,783,2478],
+              label: "Followers",
+              borderColor: "#3e95cd",
+              fill: false
+            }, { 
+              data: [282,350,411,502,635,809,947,1402,3700,5267],
+              label: "Followed",
+              borderColor: "#8e5ea2",
+              fill: false
+            }
+          ]
+        },
+        options: {
+          title: {
+            display: true,
+            text: 'Followers and Following Chart'
+          }
+        }
+      });
 });
