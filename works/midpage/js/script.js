f8bdb081051f4a64282e2760b6385420b7e4d75a
Сергій Ільчишин <sergiy.ilchyshyn@gmail.com>
2021-04-16 12:28:46 +0300

    udated midpage

---
+++ works/midpage/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    /*===== SWIPER CAROUSEL =====*/
+    const mySwiper = new Swiper(".reviews__clients", {
+        loop: true,
+        grabCursor: true,
+        pagination: {
+            el: '.swiper-pagination',
+        }
+    });
+});
