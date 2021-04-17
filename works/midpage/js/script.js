aa66c025b1f259b4825a16a6ef50bef941bc1032
Сергій Ільчишин <sergiy.ilchyshyn@gmail.com>
2021-04-17 03:00:11 +0300

    updated midpage

--- works/midpage/js/script.js
+++ works/midpage/js/script.js
@@ -1,10 +1,21 @@
 $(document).ready(function () {
     /*===== SWIPER CAROUSEL =====*/
-    const mySwiper = new Swiper(".reviews__clients", {
+    var mySwiper1 = new Swiper('.reviews__clients', {
         loop: true,
         grabCursor: true,
         pagination: {
-            el: '.swiper-pagination',
+            el: '.swiper-pagination.swiper-pagination1',
+        }
+    });
+    var mySwiper2 = new Swiper('.component-slider', {
+        loop: true,
+        grabCursor: true,
+        pagination: {
+            el: '.swiper-pagination.swiper-pagination2',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         }
     });
 });
