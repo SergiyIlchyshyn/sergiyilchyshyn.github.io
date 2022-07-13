69cda485aa6783fabf57bcffc7d3cfee190fc5ed
Sergiy Ilchyshyn <sergiy.ilchyshyn@gmail.com>
2022-07-13 10:16:20 +0300

    updated kelvin portfolio

--- works/kelvin-portfolio/main.js
+++ works/kelvin-portfolio/main.js
@@ -1,13 +1,8 @@
 const nav = document.querySelector('#nav');
 const navBtn = document.querySelector('#nav-btn');
-const navBtnImg = document.querySelector('#nav-btn-img');
 
-navBtn.onclick = () => {
-    if (nav.classList.toggle('open')) {
-        navBtnImg.src = "./img/nav-close.svg";
-    } else {
-        navBtnImg.src = "./img/nav-open.svg";
-    }
-}
+navBtn.addEventListener("click", () => {
+    nav.classList.toggle("open");
+});
 
 AOS.init();
